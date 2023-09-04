9fb8de9864499b42d9bb85dfe47bdc80ac0a1e7f
leissini <layssini15@gmail.com>
2023-09-04 11:50:43 -0700

    py

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int find(listint_t *head, int node_ptr)
+{
+	listint_t *temp = head;
+	li
+
+	while(temp)
+	{
+		temp = tmp
+/**
+ * check_cycle - check if a lincked list contains a cycle.
+ * @list: The linked list to check.
+ * Return: 1 if the list contains a cycle. 0 otherwise.
+ */
+int check_cycle(listint_t *list)
+{
+	int cycle_found = 0;
+	listint_t *previous_nodes, *temp;
+	temp = list->head;
+	while(temp && !cycle_found)
+	{
+		temp = temp->next;
+		cycle_found = node_in_array(temp, previous_node);
+	}
+}
