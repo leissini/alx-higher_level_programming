7abebefea60946d30abd69d4882c09dbb0def47c
leissini <layssini15@gmail.com>
2023-09-05 04:29:41 -0700

    if else

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for a in "abcdefghijklmnopqrstuvwxyz":
-    if(a not in "qe"):
-        print("{}".format(a), end='')
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
